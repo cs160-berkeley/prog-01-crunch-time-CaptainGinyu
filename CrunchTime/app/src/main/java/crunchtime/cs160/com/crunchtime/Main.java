58265d296032971afa08b34dde689198387f49f5
Kevin Chow <kvn_chow@berkeley.edu>
2016-01-31 21:09:44 -0800

    First commit :computer:

---
+++ CrunchTime/app/src/main/java/crunchtime/cs160/com/crunchtime/Main.java
@@ -0,0 +1,16 @@
+package crunchtime.cs160.com.crunchtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Main extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
